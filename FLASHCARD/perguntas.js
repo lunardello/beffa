f30cbd718112134febfdf35271615e81bb5be760
lunardello <isabela.lunardello@gmail.com>
2024-10-16 08:18:36 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'carro dos sonhos',
+    'g36'
+)
+
+criaCartao(
+    'pessoal',
+    'comida favorita',
+    'bife e arroz'
+)
+
+criaCartao(
+    'recomendação',
+    'estilo de roupa?',
+    'normal'
+)
+
+criaCartao(
+    'recomendacão',
+    'qual lugar favorito?',
+    'praia'
+)
